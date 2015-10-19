39bcbf13d040ffa0b0a460686229f686d4f26ff4
Your Name <beatrizcl.14@gmail.com>
2015-10-19 17:07:27 +0200

    Ejercicio 6- Finalizado y pasado pep8

--- karaoke.py
+++ karaoke.py
@@ -9,37 +9,49 @@ import json
 import urllib
 
 
-def listado(lista):
+class KaraokeLocal(smallsmilhandler.SmallSMILHandler):
 
-    for sublista in lista:
-        etiquetas = sublista[0]
-        atributos = sublista[1]
-        resultado = str(etiquetas + '\t')
-        for clave in atributos:
-            if atributos[clave]:
-                resultado = resultado + (str(clave + '="'
-                                         + atributos[clave] + '"' + '\t'))
-        print(resultado)
+    def __init__(self, fich):
 
+        parser = make_parser()
+        cHandler = smallsmilhandler.SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(fich)
+        self.My_List = cHandler.get_tags()
 
-def do_local(lista):
-    for sublista in lista:
-        atributos = sublista[1]
-        for clave in atributos:
-            if atributos[clave].startswith('http://'):
-                urllib.request.urlretrieve(atributos[clave],
-                                           atributos[clave].split('/')[-1])
+    def __str__(self):
 
+        resultado = ""
+        for sublista in self.My_List:
+            etiquetas = sublista[0]
+            atributos = sublista[1]
+            resultado += str(etiquetas + '\t')
+            for clave in atributos:
+                if atributos[clave]:
+                    resultado = resultado + (str(clave + '="'
+                                             + atributos[clave] + '"' + '\t'))
+            resultado += "\n"
+        return resultado
 
-def to_json(My_List, fich_name=""):
+    def to_json(self, fich_name=""):
 
-    if not fich_name:
-        fich_name = "local.json"
-    else:
-        fich_name = fich_name.replace(".smil", ".json")
-    with open(fich_name, 'w') as archivo_json:
-        json.dump(My_List, archivo_json, indent=4,
-                  separators=(' ', ': '), sort_keys=True)
+        if not fich_name:
+            fich_name = "local.json"
+        else:
+            fich_name = fich_name.replace(".smil", ".json")
+        with open(fich_name, 'w') as archivo_json:
+            json.dump(self.My_List, archivo_json, indent=4,
+                      separators=(' ', ': '), sort_keys=True)
+
+    def do_local(self):
+        for sublista in self.My_List:
+            atributos = sublista[1]
+            for clave in atributos:
+                if atributos[clave].startswith('http://'):
+                    urllib.request.urlretrieve(atributos[clave],
+                                               atributos[clave].split('/')[-1])
+                    atributos[clave] = atributos[clave].split('/')[-1]
+
 
 if __name__ == "__main__":
     try:
@@ -47,12 +59,9 @@ if __name__ == "__main__":
 
     except IndexError:
         print("Usage:  Python3 karaope.py file.smil")
-
-    parser = make_parser()
-    cHandler = smallsmilhandler.SmallSMILHandler()
-    parser.setContentHandler(cHandler)
-    parser.parse(fich)
-    My_List = cHandler.get_tags()
-    List = listado(My_List)
-    to_json(My_List, fich.name)
-    do_local(My_List)
+    List = KaraokeLocal(fich)
+    print(List)
+    List.to_json(fich.name)
+    List.do_local()
+    List.to_json()
+    print(List)
