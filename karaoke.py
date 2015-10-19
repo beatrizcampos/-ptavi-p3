5837a928690a8d440e986814f2b45524a5079418
beatriz campos leon <bcampos@zeta40.aulas.gsyc.es>
2015-10-19 15:15:30 +0200

    Ejercicio 4- A falta de formato JSON

--- karaoke.py
+++ karaoke.py
@@ -5,13 +5,29 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import smallsmilhandler
 import sys
+import json
 
+def listado(lista):
+
+    for sublista in lista:
+        etiquetas = sublista[0]
+        atributos = sublista[1]
+        resultado = str(etiquetas + '\t')        
+        for clave in atributos:
+            if atributos[clave]:
+                resultado = resultado + (str(clave + '="' + atributos[clave] + '"' + '\t'))
+        print(resultado)    
 
 if __name__ == "__main__":
     try:
-        fich = open(sys.argv[1], 'r')
+        fich = open(sys.argv[1])
 
     except IndexError: 
         print("Usage:  Python3 karaope.py file.smil")
 
-  
+    parser = make_parser()
+    cHandler = smallsmilhandler.SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(fich)
+    My_List = cHandler.get_tags()
+    List = listado(My_List)
